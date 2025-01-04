9258679756602aecbc5f470eae23955d0289ccd3
Vishal kumar <vishalkumar756380@gmail.com>
2025-01-04 12:04:00 +0530

    hgkj

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MacContainer from './macContainer'
+import MacContainer from './MacContainer'
 import { Canvas } from '@react-three/fiber'
 import { Environment, OrbitControls, ScrollControls } from '@react-three/drei'
 
 function App() {
-  const [count, setCount] = useState(0)
  
   return (
     <div className='h-screen w-full'>
